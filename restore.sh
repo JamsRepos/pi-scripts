1edcc522417cbaa3a917c31bd39e8d947bf268e0
James Finch <LubricantJam@users.noreply.github.com>
2023-01-04 03:13:52 +0000

    Optimised the restore script.

--- restore.sh
+++ restore.sh
@@ -7,11 +7,8 @@ app_data_dir="/opt/appdata"
 # Set the mount point
 mount="idrive:backups"
 
-# Array to store the file names
-declare -a files
-
 # Read all file names in the mount point
-# and store them in the array
+files=()
 while read -r line; do
   files+=("$line")
 done < <(rclone lsf "$mount")
@@ -31,28 +28,23 @@ container_name="${file%.*.*}"
 # Ask if the restore should be local or remote
 read -p "Local or remote restore? [l/r] " restore_type
 
-# If the restore is local and the file exists, untar the file in the app data directory
-# and start the docker container
-if [[ "$restore_type" == "l" && -f "$backup_dir/$file" ]]; then
-  echo "Extracting $file in $app_data_dir..."
-  tar -xvf "$backup_dir/$file" -C "$app_data_dir" &>/dev/null
-  cd "$app_data_dir/$container_name"
-  echo "Finally, restoring $container_name!"
-  docker-compose up -d 
-# If the restore is local and the file does not exist, prompt the user to either select
-# a remote restore or cancel
-elif [[ "$restore_type" == "l" && ! -f "$backup_dir/$file" ]]; then
-  read -p "File does not exist. Select 'r' for remote restore or 'c' to cancel: " choice
-  if [[ "$choice" == "r" ]]; then
-    restore_type="r"
+if [[ "$restore_type" == "l" ]]; then
+  if [[ -f "$backup_dir/$file" ]]; then
+    echo "Extracting $file in $app_data_dir..."
+    tar -xvf "$backup_dir/$file" -C "$app_data_dir" &>/dev/null
+    cd "$app_data_dir/$container_name"
+    echo "Finally, restoring $container_name!"
+    docker-compose up -d
   else
-    exit
+    read -p "File does not exist. Select 'r' for remote restore or 'c' to cancel: " choice
+    if [[ "$choice" == "r" ]]; then
+      restore_type="r"
+    else
+      exit
+    fi
   fi
 fi
 
-# If the restore is remote, use rclone to copy the file
-# to the app data directory and untar it there, then start the
-# docker container
 if [[ "$restore_type" == "r" ]]; then
   echo "Copying $file from $mount to $backup_dir..."
   rclone copy "$mount/$file" "$backup_dir"

3af1e3b8da0f2980b5d67d9df48eadab83092de9
James Finch <LubricantJam@users.noreply.github.com>
2023-01-04 02:56:22 +0000

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -4,14 +4,17 @@
 backup_dir="/opt/backups"
 app_data_dir="/opt/appdata"
 
+# Set the mount point
+mount="idrive:backups"
+
 # Array to store the file names
 declare -a files
 
-# Read all file names in the backup directory
+# Read all file names in the mount point
 # and store them in the array
 while read -r line; do
   files+=("$line")
-done < <(find "$backup_dir" -type f -printf "%f\n")
+done < <(rclone lsf "$mount")
 
 # Display menu options for each file
 PS3='Select a file to restore: '
@@ -28,18 +31,34 @@ container_name="${file%.*.*}"
 # Ask if the restore should be local or remote
 read -p "Local or remote restore? [l/r] " restore_type
 
-# If the restore is local, untar the file in the app data directory
+# If the restore is local and the file exists, untar the file in the app data directory
 # and start the docker container
-if [[ "$restore_type" == "l" ]]; then
-  tar -xvf "$backup_dir/$file" -C "$app_data_dir"
+if [[ "$restore_type" == "l" && -f "$backup_dir/$file" ]]; then
+  echo "Extracting $file in $app_data_dir..."
+  tar -xvf "$backup_dir/$file" -C "$app_data_dir" &>/dev/null
   cd "$app_data_dir/$container_name"
+  echo "Finally, restoring $container_name!"
   docker-compose up -d 
+# If the restore is local and the file does not exist, prompt the user to either select
+# a remote restore or cancel
+elif [[ "$restore_type" == "l" && ! -f "$backup_dir/$file" ]]; then
+  read -p "File does not exist. Select 'r' for remote restore or 'c' to cancel: " choice
+  if [[ "$choice" == "r" ]]; then
+    restore_type="r"
+  else
+    exit
+  fi
+fi
+
 # If the restore is remote, use rclone to copy the file
 # to the app data directory and untar it there, then start the
 # docker container
-elif [[ "$restore_type" == "r" ]]; then
-  rclone copy idrive:backups/"$file" "$backup_dir"
-  tar -xvf "$backup_dir/$file" -C "$app_data_dir"
+if [[ "$restore_type" == "r" ]]; then
+  echo "Copying $file from $mount to $backup_dir..."
+  rclone copy "$mount/$file" "$backup_dir"
+  echo "Extracting $file in $app_data_dir..."
+  tar -xvf "$backup_dir/$file" -C "$app_data_dir" &>/dev/null
   cd "$app_data_dir/$container_name"
+  echo "Finally, restoring $container_name!"
   docker-compose up -d
 fi
